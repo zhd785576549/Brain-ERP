de2a421fe5ec2c2c14313a27d5eb2fab3fe5aa14
张冬 <zhangdong@embway.com>
2017-10-19 13:29:03 +0800

    modify

--- website/member/static/forms.js
+++ website/member/static/forms.js
@@ -2,38 +2,36 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     jQuery.i18n.properties({
-        name:'Messages', 
-        path:'/static/i18n/', 
-        mode:'both',
-        language:'en',
+        name: 'Messages',
+        path: '/static/i18n/',
+        mode: 'both',
         async: true,
-        callback: function() {
-
-        }
-    });
-    
-    $('#loginForm').validate({
-        rules: {
-            email: {
-                required: true,
-                email: true
-            },
-            password: {
-                required: true,
-                minLength: 6,
-                maxLength: 20
-            }
-        },
-        messages: {
-            email: {
-                required: jQuery.i18n.prop('msg_email_required'),
-                email: jQuery.i18n.prop('msg_email_format_error')
-            },
-            password: {
-                required: jQuery.i18n.prop('msg_password_required'),
-                minLength: jQuery.i18n.prop('msg_password_min_length'),
-                maxLength: jQuery.i18n.prop('msg_password_max_length')
-            }
+        debug: false,
+        callback: function () {
+            $('#loginForm').validate({
+                rules: {
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true,
+                        minLength: 6,
+                        maxLength: 20
+                    }
+                },
+                messages: {
+                    email: {
+                        required: jQuery.i18n.prop('msg_email_required'),
+                        email: jQuery.i18n.prop('msg_email_format_error')
+                    },
+                    password: {
+                        required: jQuery.i18n.prop('msg_password_required'),
+                        minLength: jQuery.i18n.prop('msg_password_min_length'),
+                        maxLength: jQuery.i18n.prop('msg_password_max_length')
+                    }
+                }
+            });
         }
     });
 })
