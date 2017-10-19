9c8b3fc35cff893485c6e66f622a218a70ba893b
张冬 <zhangdong@embway.com>
2017-10-19 11:21:33 +0800

    add jquery i18n, change languge by browser setting of main language

--- website/member/static/forms.js
+++ website/member/static/forms.js
@@ -1,3 +1,39 @@
 $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    jQuery.i18n.properties({
+        name:'Messages', 
+        path:'/static/i18n/', 
+        mode:'both',
+        language:'en',
+        async: true,
+        callback: function() {
+
+        }
+    });
     
+    $('#loginForm').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minLength: 6,
+                maxLength: 20
+            }
+        },
+        messages: {
+            email: {
+                required: jQuery.i18n.prop('msg_email_required'),
+                email: jQuery.i18n.prop('msg_email_format_error')
+            },
+            password: {
+                required: jQuery.i18n.prop('msg_password_required'),
+                minLength: jQuery.i18n.prop('msg_password_min_length'),
+                maxLength: jQuery.i18n.prop('msg_password_max_length')
+            }
+        }
+    });
 })
